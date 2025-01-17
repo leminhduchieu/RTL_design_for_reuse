c5a10a1b3ebe4d2643fe1c8a65b501ba62d56da9
Le Minh Duc Hieu <111422576+leminhduchieu@users.noreply.github.com>
2025-01-17 22:50:11 +0700

    Create template.v

---
+++ template.v
@@ -0,0 +1,31 @@
+/*********************************************************/
+// MODULE: Code template
+//
+// FILE NAME: template.v
+// VERSION: 1.0
+// DATE: Jan 17, 2025
+// AUTHOR: Le Hieu
+//
+// CODE TYPE: RTL or Behavioral Level
+//
+// DESCRIPTION: This template is used to have a coding
+// standard for each Verilog function.
+//
+/*********************************************************/
+// DEFINES
+
+// TOP MODULE
+
+// INPUTS
+
+// OUTPUTS
+
+// INOUTS
+
+// SIGNAL DECLARATIONS
+
+// PARAMETERS
+
+// ASSIGN STATEMENTS
+
+// MAIN CODE
